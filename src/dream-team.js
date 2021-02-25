9bef315f4cb33c62c7b7a6a75b98969880fe1c1b
eugenemp <phoenixlp91@mail.ru>
2021-02-25 17:07:10 +0300

    feat: implement createDreamTeam function

--- src/dream-team.js
+++ src/dream-team.js
@@ -1,6 +1,25 @@
 const CustomError = require("../extensions/custom-error");
 
-module.exports = function createDreamTeam(/* members */) {
-  throw new CustomError('Not implemented');
-  // remove line with error and write your code here
+module.exports = function createDreamTeam(team) {
+
+  //Return false on wrong type
+  if(!Array.isArray(team)) return false;
+
+  let str;
+  let result = [];
+    
+  team.forEach(element => {
+
+    //Check if current value of the aray is string...
+    if(typeof element == 'string') {
+  
+      //...if so, remove whitespaces and pupulate array with chars
+      //converted to uppercase
+      str = element.replace(/\s+/g, '');
+      result.push(str.charAt(0).toUpperCase());
+    }
+  });
+
+  //Sort characters in result array, convert to string and return
+  return result.sort().join('');
 };
