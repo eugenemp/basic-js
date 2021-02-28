6b09cc0a7cbf4a56729e14f1b3c772672eb5498a
eugenemp <phoenixlp91@mail.ru>
2021-02-28 22:47:16 +0300

    fix: grammar mistakes in comments on transform function

--- src/transform-array.js
+++ src/transform-array.js
@@ -5,7 +5,7 @@ module.exports = function transform(arr) {
   //Throw an Error if type is wrong or return empty array if passed one is also empty
   if(!Array.isArray(arr)) throw Error;
   if(arr.length === 0) return [];
-  //Clone original array so it will stay anaffected
+  //Clone original array so it will stay unaffected
   let temp = [];
   let result = [];
   
@@ -17,7 +17,7 @@ module.exports = function transform(arr) {
     //Once sequence is found:
     //Check if element next or previous to control sequence is not 'undefind'
     //If it is true, delete control sequence and make changes to specified element
-    //If it is false, so item near control sequence is 'underfined', delete only control sequence
+    //If it is false, so item near control sequence is 'undefined', delete only control sequence
     //Use 'delete' command or copy element if needed to preserve array's length while making changes    
     if(temp[i] === '--discard-next') temp[i + 1] !== undefined ? 
     (delete temp[i], delete temp[i + 1]) : delete temp[i];
