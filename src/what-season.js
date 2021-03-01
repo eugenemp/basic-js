84520b592f79bd262e1d5df9681ba6f1aa1f6316
eugenemp <phoenixlp91@mail.ru>
2021-03-01 03:08:51 +0300

    style: reformat code

--- src/what-season.js
+++ src/what-season.js
@@ -1,20 +1,19 @@
 const CustomError = require("../extensions/custom-error");
 
 module.exports = function getSeason(date) {
-
-  //Check if argument is passed to the function
-  if(!date) return 'Unable to determine the time of year!';
   
+  //Check if argument is passed to the function
+  if(!date) return 'Unable to determine the time of year!';  
     //Check if passed argument is a real Date object and throw an error if not
     if(Object.prototype.toString.call(date) !== "[object Date]" ) {
       throw Error;
-    };
-  
+    };  
     //Get month value and return corresponding season
     let month = date.getMonth();
+
+    return (month > 10 || month < 2) ? 'winter' :
+                         (month < 5) ? 'spring' :
+                         (month < 8) ? 'summer' :
+                                       'autumn' ;
     
-    if(month > 10 || month < 2) return 'winter';
-    if(month > 1 && month < 5) return 'spring';
-    if(month > 4 && month < 8) return 'summer';
-    if(month > 7 && month < 11) return 'autumn';
 };
