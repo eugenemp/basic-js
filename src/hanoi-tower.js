a13a466c2af260bb55b4ff17db0b84aeafcdacad
eugenemp <phoenixlp91@mail.ru>
2021-03-01 21:56:06 +0300

    feat: implement calculateHanoi function

--- src/hanoi-tower.js
+++ src/hanoi-tower.js
@@ -1,6 +1,14 @@
 const CustomError = require("../extensions/custom-error");
 
-module.exports = function calculateHanoi(/* disksNumber, turnsSpeed */) {
-  throw new CustomError('Not implemented');
-  // remove line with error and write your code here
+module.exports = function calculateHanoi(disksNumber, turnsSpeed) {
+  //The formula to find the number of steps needed to move all disks to 
+  //another rod is 2^n - 1, where 'n' is the disks amount:
+  let steps = Math.pow(2, disksNumber) - 1;
+  // * Calculate the time needed to one turn with specified speed
+  // * Multiply it by amount of previously found steps
+  // * Also multiply result by 60 to find time in seconds
+  // * Round down result
+  let time = Math.floor(60 / turnsSpeed * steps * 60);
+  //Return array of results
+  return {turns: steps, seconds: time};
 };
