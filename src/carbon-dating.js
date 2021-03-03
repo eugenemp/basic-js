80b21be53880d0aade79f85906d2b142e48ef324
eugenemp <phoenixlp91@mail.ru>
2021-03-03 03:09:07 +0300

    feat: implement dateSample function

--- src/carbon-dating.js
+++ src/carbon-dating.js
@@ -2,8 +2,24 @@ const CustomError = require("../extensions/custom-error");
 
 const MODERN_ACTIVITY= 15; 
 const HALF_LIFE_PERIOD= 5730;
+const LN2= 0.693;
 
-module.exports = function dateSample(/* sampleActivity */) {
-  throw new CustomError('Not implemented');
-  // remove line with error and write your code here
+module.exports = function dateSample(sampleActivity) {
+  // * Check if argument type is 'string' and there is a only number in it
+  // * Check range of the sample activity value 
+  if(
+    typeof(sampleActivity) !== 'string' ||
+    isNaN(parseFloat(sampleActivity))   ||
+    parseFloat(sampleActivity) <= 0     ||
+    parseFloat(sampleActivity) > 15
+
+    ) return false;
+  //Convert argument to 'number'
+  sampleActivity = parseFloat(sampleActivity);
+  //Calculate rate constant for the reaction
+  let k = LN2 / HALF_LIFE_PERIOD;
+  //Calculate sample approximate age
+  let result = Math.log(MODERN_ACTIVITY / sampleActivity) / k;
+  //Finally return rounded up result
+  return Math.ceil(result);
 };
